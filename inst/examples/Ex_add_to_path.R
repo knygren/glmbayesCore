26e356f61dc2600d2fdfe15cffa7f09abee70674
knygren <kjell.a.nygren@gmail.com>
2026-04-29 20:42:26 -0400

    Replace dontrun examples with CRAN-safe guards

--- inst/examples/Ex_add_to_path.R
+++ inst/examples/Ex_add_to_path.R
@@ -1,9 +1,11 @@
 ############################### Start of add_to_path example ####################
 
-\dontrun{
-add_to_path_windows(c("C:/OpenCL/bin"))
-add_to_path_linux(c("/usr/local/cuda/bin"))
-add_to_libpath_linux(c("/usr/local/cuda/lib64"))
+\donttest{
+if (interactive()) {
+  add_to_path_windows(c("C:/OpenCL/bin"))
+  add_to_path_linux(c("/usr/local/cuda/bin"))
+  add_to_libpath_linux(c("/usr/local/cuda/lib64"))
+}
 }
 
 ###############################################################################
--- inst/examples/Ex_load_kernel_source.R
+++ inst/examples/Ex_load_kernel_source.R
@@ -1,9 +1,11 @@
 ############################### Start of load_kernel_source example ####################
 
-\dontrun{
+\donttest{
 if (has_opencl()) {
   src <- load_kernel_source("nmath/bd0.cl")
   lib <- load_kernel_library("nmath")
+  nchar(src)
+  nchar(lib)
 }
 }
 
